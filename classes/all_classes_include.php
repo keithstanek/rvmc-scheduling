67c5948d9ea9229ecf52157ef8d712e570445d65
kstane <kstane@acxiom.com>
2021-09-15 14:50:39 -0500

    adding dao class and an example on how to use it

--- classes/all_classes_include.php
+++ classes/all_classes_include.php
@@ -3,5 +3,6 @@
 require dirname(__FILE__) . "/../config/constants.php";
 require dirname(__FILE__) . "/../config/db_conn.php";
 require dirname(__FILE__) . "/../config/session.php";
+require dirname(__FILE__) . "/test_dao.php";
 
 ?>
--- index.php
+++ index.php
@@ -1,35 +1,22 @@
 <?php
 require dirname(__FILE__) . "/classes/all_classes_include.php";
-
 ?>
 
-Test Database connection<br>
+
 
 <?php
-try {
-    $db = DbUtil::getConnection();
-
-    $sql = "select * from test";
-    $stmt = $db->prepare($sql);
-    if ($stmt->execute()) {
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            ?>
-                id [<?= $row["id"] ?>]
-                first [<?= $row["first"] ?>]
-                middle [<?= $row["middle"] ?>]
-                last [<?= $row["last"] ?>]
-                <br>
-            <?php
-        }
-    }
-
-    $db = null;
-} catch (PDOException $e) {
-    echo 'DATABASE ERROR' . $e->getMessage() . '<br>';
-}
-
+// add the test table here, the same would be for headers and footers of your templates
+require dirname(__FILE__) . "/page-includes/test-table-data.php";
 ?>
+
+<br>
+<br>
+
 <hr>
+
+<br>
+<br>
+
 <?php
 phpinfo();
 ?>
