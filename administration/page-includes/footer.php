49882fa1d73492da1f2e3c6780dbe603b15139ac
kstane <kstane@acxiom.com>
2021-09-22 11:04:45 -0500

    initial setup for php admin template

---
+++ administration/page-includes/footer.php
@@ -0,0 +1,18 @@
+                <footer class="footer text-center">
+                    All Rights Reserved by Xtreme Admin. Designed and Developed by <a href="https://www.wrappixel.com">WrapPixel</a>.
+                </footer>
+
+            </div>
+        </div>
+        <script src="assets/libs/jquery/dist/jquery.min.js"></script>
+        <!-- Bootstrap tether Core JavaScript -->
+        <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/app-style-switcher.js"></script>
+        <!--Wave Effects -->
+        <script src="assets/js/waves.js"></script>
+        <!--Menu sidebar -->
+        <script src="assets/js/sidebarmenu.js"></script>
+        <!--Custom JavaScript -->
+        <script src="assets/js/custom.js"></script>
+    </body>
+</html>
---
+++ administration/page-includes/page-wrapper-start.php
@@ -0,0 +1,17 @@
+<div class="page-wrapper">
+    <div class="page-breadcrumb">
+        <div class="row align-items-center">
+            <div class="col-12">
+                <h4 class="page-title">Starter Page</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Library</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
