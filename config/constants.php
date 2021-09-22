ec2bbee357a3878fd0ab508f8f30b6d0abb566b9
kstane <kstane@acxiom.com>
2021-09-22 10:28:23 -0500

    initial admin template upload

--- config/constants.php
+++ config/constants.php
@@ -5,15 +5,15 @@ class Constants {
   // DEVELOPMENT CONNECTION INFORMATION
   //************************************************************************************************
 
-  public static $DATABASE_HOST     = "db5004907070.hosting-data.io";
-  public static $DATABASE_USERNAME = "dbu56069";
-  public static $DATABASE_PASSWORD = "T@terM@ker1";
-  public static $DATABASE_NAME     = "dbs4110508";
+//  public static $DATABASE_HOST     = "db5004907070.hosting-data.io";
+//  public static $DATABASE_USERNAME = "dbu56069";
+//  public static $DATABASE_PASSWORD = "T@terM@ker1";
+//  public static $DATABASE_NAME     = "dbs4110508";
 
-//  public static $DATABASE_HOST     = "127.0.0.1";
-//  public static $DATABASE_USERNAME = "ubx";
-//  public static $DATABASE_PASSWORD = "test";
-//  public static $DATABASE_NAME     = "ubx";
+  public static $DATABASE_HOST     = "127.0.0.1";
+  public static $DATABASE_USERNAME = "ubx";
+  public static $DATABASE_PASSWORD = "test";
+  public static $DATABASE_NAME     = "ubx";
 
 
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
